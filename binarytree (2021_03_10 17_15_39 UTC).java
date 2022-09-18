1d9f9915d9ff3a1f63a263b6a3b4d0da29aa9efc
aariascruz1 <113816778+aariascruz1@users.noreply.github.com>
2022-09-17 22:19:31 -0700

    Add files via upload

---
+++ binarytree (2021_03_10 17_15_39 UTC).java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class binarytree<E>{
+    E data;
+    binarytree<E> left;
+    binarytree<E> right;
+    public binarytree(E data) {
+        this.data = data;
+
+    }}
